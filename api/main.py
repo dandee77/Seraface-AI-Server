22bda3eee2dd3039466bc926fb9c9713c8b8ad96
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-30 21:33:01 +0800

    update main.py to bind uvicorn to localhost and add phase2.py as a new file

--- api/main.py
+++ api/main.py
@@ -7,4 +7,4 @@ app.include_router(phase1_router, prefix="/phase1", tags=["Phase 1"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
